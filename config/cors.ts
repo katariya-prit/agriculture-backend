1f5aee3dc4a4fd3973a5f4ec38eae058b9450c59
Mr-pk-ahir <pritahir123098@gmail.com>
2026-09-03 18:26:32 +0530

    perfect set url

--- config/cors.ts
+++ config/cors.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@adonisjs/cors'
 
 const corsConfig = defineConfig({
   enabled: true,
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // tamaru Vite port match karo
+  origin: ['https://agriculture-web-inky.vercel.app', 'http://localhost:5173'], // tamaru Vite port match karo
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
   headers: true,
   exposeHeaders: ['cache-control', 'content-language', 'content-type', 'expires', 'pragma'],
