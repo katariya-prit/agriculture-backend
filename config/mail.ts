bfeb996ce87d9fad34ab4c5048bd5f490ab7f93b
Mr-pk-ahir <pritahir123098@gmail.com>
2026-09-11 21:49:26 +0530

    add mail seting

--- config/mail.ts
+++ config/mail.ts
@@ -4,34 +4,19 @@ import { defineConfig, transports } from '@adonisjs/mail'
 const mailConfig = defineConfig({
   default: env.get('MAIL_MAILER'),
 
-  /**
-   * The mailers object can be used to configure multiple mailers
-   * each using a different transport or same transport with different
-   * options.
-   */
   from: {
     address: env.get('MAIL_FROM_ADDRESS'),
     name: env.get('MAIL_FROM_NAME'),
   },
 
-  /**
-   * The globals are shared with all the templates rendered using the
-   * configured template engine.
-   */
   globals: {
     brandName: 'FarmLoop',
   },
 
-  /**
-   * The mailers object can be used to configure multiple mailers
-   * each using a different transport or same transport with different
-   * options.
-   */
   mailers: {
     smtp: transports.smtp({
       host: env.get('SMTP_HOST'),
       port: env.get('SMTP_PORT'),
-      // Brevo ne authentication FARRJIYAT joie — comment kadhi nakhyu
       auth: {
         type: 'login',
         user: env.get('SMTP_USERNAME'),
